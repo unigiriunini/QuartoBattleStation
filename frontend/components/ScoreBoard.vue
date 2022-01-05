c770afa7877fb839f4bc8b22dd42bcf200fb937f
Unigiri Unini <me@unigiri.net>
2022-01-05 17:16:47 +0900

    Format an ELO table with Tailwind CSS

--- frontend/components/ScoreBoard.vue
+++ frontend/components/ScoreBoard.vue
@@ -1,16 +1,39 @@
 <template>
-  <v-data-table :headers="headers" :items="ais" :items-per-page="10" :loading=loading :loading-text=loadingText></v-data-table>
+  <div class="flex justify-center items-center">
+    <div class="border px-10 py-10 mt-10 rounded overflow-hidden shadow-lg">
+      <table class="table-auto">
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th v-for="column_name in column_names" class="px-4 py-2">{{ getColumnName(column_name) }}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(ai, index) in ais" :key="ai.name" :class="index % 2 ? 'bg-gray-100' : ''">
+            <td class="px-4 py-2 border">{{ index + 1 }}</td>
+            <td v-for="column_name in column_names" class="px-4 py-2 border">{{ ai[column_name] }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  props: ['ais', 'loading', 'loadingText'],
-  data () {
+  props: ['ais'],
+  data() {
     return {
-      headers: [
-        { text: 'name', value: 'name' },
-        { text: 'ELO', value: 'elo' },
-      ]
+      column_names: ['name', 'elo']
+    }
+  },
+  methods: {
+    getColumnName(column_name) {
+      if (column_name == 'elo') {
+        return column_name.toUpperCase()
+      } else {
+        return column_name.charAt(0).toUpperCase() + column_name.slice(1)
+      }
     }
   }
 }
