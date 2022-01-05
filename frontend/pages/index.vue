4024d6d88745fd07065f675a178a387636449767
Unigiri Unini <me@unigiri.net>
2022-01-05 17:15:34 +0900

    Subscribe ELOs

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,23 +1,34 @@
 <template>
-  <ScoreBoard :ais=ais :loading=loading :loadingText=loadingText />
+  <ScoreBoard :ais=ais />
 </template>
 
 <script>
 export default {
   data() {
     return {
-      loading: true,
-      loadingText: 'Now loading...',
+      ais: [],
+      snapshots: []
     }
   },
-  asyncData({ $fire }) {
-    return $fire.firestore.collection('AIs').get().then((snapshot) => {
-      return {
-        ais: snapshot.docs.map((doc) => { return { name: doc.id, elo: doc.data().ELO } })
-      }
-    }).catch((error) => {
-      return { loadingText: 'Sorry, something went wrong' }
-    })
+  created() {
+    this.snapshots.push(
+      this.$fire.firestore.collection('AIs').onSnapshot((snapshot) => {
+        this.ais = snapshot.docs.map((doc) => { return { name: doc.id, elo: doc.data().ELO } }).sort((a, b) => b.elo - a.elo)
+      })
+    )
+  },
+  mounted() {
+    window.addEventListener("beforeunload", this.unsubscribeSnapshots)
+  },
+  beforeDestroy() {
+    this.unsubscribeSnapshots()
+    window.removeEventListener("beforeunload", this.unsubscribeSnapshots)
+  },
+  methods: {
+    unsubscribeSnapshots() {
+      console.log(`Unsubscribe ${this.snapshots.length} snapshots`)
+      this.snapshots.forEach((unsubscribe) => unsubscribe())
+    }
   }
 }
 </script>
